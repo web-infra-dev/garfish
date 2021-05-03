576cff5ba9efa7f754f0df0b3966e2fe07d6cd9f
zhoushaw <codingzx@gmail.com>
2021-05-03 18:32:42 +0800

    chore: modify vm-sandbox

--- packages/runtime/plugins/browser-vm/src/index.ts
+++ packages/runtime/plugins/browser-vm/src/index.ts
@@ -1,36 +1,17 @@
-import Garfish, { Plugin } from '@garfish/core';
+import Garfish, { interfaces } from '@garfish/core';
 import { assert, warn } from '@garfish/utils';
 
-declare module '@garfish/core' {
-  export default interface Garfish {
-    setExternal: (
-      nameOrExtObj: string | Record<string, any>,
-      value?: any,
-    ) => void;
-    externals: Record<string, any>;
-  }
-}
+// declare module '@garfish/core' {
+//   export default interface Garfish {
+//     setExternal: (
+//       nameOrExtObj: string | Record<string, any>,
+//       value?: any,
+//     ) => void;
+//     externals: Record<string, any>;
+//   }
+// }
 
-export default function addCjsExternalPlugin(Garfish: Garfish): Plugin {
-  Garfish.setExternal = setExternal;
-  console.log(Garfish);
-  function setExternal(
-    nameOrExtObj: string | Record<string, any>,
-    value?: any,
-  ) {
-    assert(nameOrExtObj, 'Invalid parameter.');
-    if (typeof nameOrExtObj === 'object') {
-      for (const key in nameOrExtObj) {
-        if (Garfish.externals[key]) {
-          __DEV__ && warn(`The "${key}" will be overwritten in external.`);
-        }
-        Garfish.externals[key] = nameOrExtObj[key];
-      }
-    } else {
-      Garfish.externals[nameOrExtObj] = value;
-    }
-  }
-
+export default function BrowserVm(_Garfish: Garfish): interfaces.Plugin {
   return {
     name: 'bb',
     initialize() {
