e7ad8ae0507d32ff23e8e86f2ecfd25e38a48768
zhoushaw <codingzx@gmail.com>
2021-06-21 23:22:14 +0800

    chore: delete unelss type file

--- dev/main/garfish.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="./node_modules/@garfish/browser-vm" />
-/// <reference types="./node_modules/@garfish/browser-snapshot" />
-/// <reference types="./node_modules/@garfish/router" />
