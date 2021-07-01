2a8a549961f6da9923a1073687e16a2489a64d8e
chentao.arthur <chentao.arthur@bytedance.com>
2021-07-01 21:24:31 +0800

    feat: delete unless code

--- packages/runtime/remote-component/src/common.ts
+++ packages/runtime/remote-component/src/common.ts
@@ -21,25 +21,21 @@ export const externals: Record<PropertyKey, any> = garfishGlobalEnv
   ? { ...garfishGlobalEnv.externals }
   : {};
 
-const defaultOptions: Pick<ComponentInfo, 'cache'> = {
-  cache: true,
-};
-
 export const loader: Loader = (() => {
-  // @ts-ignore
   if (garfishGlobalEnv) {
     const loader = garfishGlobalEnv.loader;
     // Garfish loader will have an identifier
     if (loader && loader.personalId === Symbol.for('garfish.loader')) {
       return loader;
     }
-    return new Loader();
   }
+  return new Loader();
 })();
 
 export const purifyOptions = (options: ComponentInfo | string) => {
   if (typeof options === 'string') options = { url: options };
-  return deepMerge(defaultOptions, options || {}) as ComponentInfo;
+  // Default use cache
+  return deepMerge({ cache: true }, options || {}) as ComponentInfo;
 };
 
 export const getComponentCode = (url: string) => {
