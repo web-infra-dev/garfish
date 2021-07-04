a39768e89fd7f8cf37a25e972c48bcea3778d2ea
chentao.arthur <chentao.arthur@bytedance.com>
2021-07-04 17:00:33 +0800

    feat: perfect type gymnastics

--- packages/runtime/remote-module/src/apis/esModule.ts
+++ packages/runtime/remote-module/src/apis/esModule.ts
@@ -1,15 +1,20 @@
 import { isObject, isPromise } from '@garfish/utils';
 
-type EsModuleResult<T> = {
+type ESModuleResult<T> = {
   default: T;
   __esModule: true;
 };
 
-export function esModule<T>(obj: T) {
-  if (isObject(obj) && (obj as any).__esModule === true) {
+export function esModule<T extends Promise<any>>(
+  obj: T,
+): Promise<ESModuleResult<T extends Promise<infer P> ? P : T>>;
+export function esModule<T extends ESModuleResult<any>>(obj: T): T;
+export function esModule<T>(obj: T): ESModuleResult<T>;
+export function esModule(obj: any) {
+  if (isObject(obj) && obj.__esModule === true) {
     return obj;
   } else if (isPromise(obj)) {
-    return (obj as any).then(esModule) as Promise<EsModuleResult<T>>;
+    return obj.then(esModule);
   } else {
     const esm = { default: obj };
     Object.defineProperty(esm, '__esModule', { value: true });
