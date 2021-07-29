4930269a38812ad8e57bea56392cc691e51d1f06
chentao.arthur <chentao.arthur@bytedance.com>
2021-07-29 20:30:58 +0800

    fix: adjust the timing of repair url

--- packages/runtime/browser-vm/src/dynamicNode/processor.ts
+++ packages/runtime/browser-vm/src/dynamicNode/processor.ts
@@ -39,11 +39,6 @@ export class DynamicNodeProcessor {
     this.DOMApis = new DOMApis(sandbox.global.document);
     this.rootElement = rootElm(this.sandbox) || document;
     this.tagName = el.tagName ? el.tagName.toLowerCase() : '';
-
-    // Deal with some static resource nodes
-    if (sourceListTags.includes(this.tagName)) {
-      this.fixResourceNodeUrl();
-    }
   }
 
   private is(tag: string) {
@@ -208,6 +203,11 @@ export class DynamicNodeProcessor {
       this.sandbox.deferClearEffects.add(() => {
         this.DOMApis.removeElement(this.el);
       });
+    }
+
+    // Deal with some static resource nodes
+    if (sourceListTags.includes(this.tagName)) {
+      this.fixResourceNodeUrl();
     }
 
     // Add dynamic script node by loader
