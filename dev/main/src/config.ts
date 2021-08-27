2d70aaf19780f2da8419ea06f6c61347b2c54f24
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2021-08-27 17:09:44 +0800

    chore: Optimizing the startup command

--- dev/main/src/config.ts
+++ dev/main/src/config.ts
@@ -1,55 +1,55 @@
-// import GarfishInstance, { interfaces } from 'garfish';
-// import './monitoring';
+import GarfishInstance, { interfaces } from 'garfish';
+import './monitoring';
 
-// (window as any).__GARFISH_PARENT__ = true;
+(window as any).__GARFISH_PARENT__ = true;
 
-// // let asyncTime = function () {
-// //   return new Promise((resolve) => {
-// //     setTimeout(() => {
-// //       resolve(true);
-// //     }, 3000);
-// //   });
-// // };
-// let defaultConfig: interfaces.Options = {
-//   basename: '/garfish_master',
-//   domGetter: () => {
-//     // await asyncTime();
-//     return document.querySelector('#submoduleByRouter');
-//   },
-//   apps: [
-//     {
-//       name: 'react',
-//       activeWhen: '/react',
-//       // cache: true,
-//       entry: 'http://localhost:2444',
-//     },
-//     {
-//       name: 'vue',
-//       activeWhen: '/vue',
-//       // cache: true,
-//       entry: 'http://localhost:2666',
-//     },
-//     {
-//       name: 'vue2',
-//       activeWhen: '/vue2',
-//       // cache: true,
-//       entry: 'http://localhost:2777',
-//     },
-//   ],
-//   autoRefreshApp: true,
-//   disablePreloadApp: true,
-//   protectVariable: ['MonitoringInstance', 'Garfish'],
-//   // sandbox: {
-//   //   snapshot: false
-//   // },
+// let asyncTime = function () {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(true);
+//     }, 3000);
+//   });
 // };
+let defaultConfig: interfaces.Options = {
+  basename: '/garfish_master',
+  domGetter: () => {
+    // await asyncTime();
+    return document.querySelector('#submoduleByRouter');
+  },
+  apps: [
+    {
+      name: 'react',
+      activeWhen: '/react',
+      // cache: true,
+      entry: 'http://localhost:2444',
+    },
+    {
+      name: 'vue',
+      activeWhen: '/vue',
+      // cache: true,
+      entry: 'http://localhost:2666',
+    },
+    {
+      name: 'vue2',
+      activeWhen: '/vue2',
+      // cache: true,
+      entry: 'http://localhost:2777',
+    },
+  ],
+  autoRefreshApp: true,
+  disablePreloadApp: true,
+  protectVariable: ['MonitoringInstance', 'Garfish'],
+  // sandbox: {
+  //   snapshot: false
+  // },
+};
 
-// // The test environment into
-// if (typeof Cypress !== 'undefined') {
-//   defaultConfig = {
-//     ...defaultConfig,
-//     ...Cypress.env().garfishRunConfig,
-//   };
-// }
+// The test environment into
+if (typeof Cypress !== 'undefined') {
+  defaultConfig = {
+    ...defaultConfig,
+    ...Cypress.env().garfishRunConfig,
+  };
+}
 
-// export const Config = defaultConfig;
+export const Config = defaultConfig;
--- dev/main/src/monitoring.ts
+++ dev/main/src/monitoring.ts
@@ -1,56 +1,56 @@
-// import ConfigCommon from '../../config.json';
+import ConfigCommon from '../../config.json';
 
-// declare global {
-//   interface Window {
-//     fetchM: Function;
-//     unhandledrejectionError: Function;
-//     normalError: Function;
-//     DynamicResource: Function;
-//     resourceError: Function;
-//   }
-// }
+declare global {
+  interface Window {
+    fetchM: Function;
+    unhandledrejectionError: Function;
+    normalError: Function;
+    DynamicResource: Function;
+    resourceError: Function;
+  }
+}
 
-// window.fetchM = function fetchM() {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('get', `http://localhost:${ConfigCommon.mainPort}/mainApp`, true);
-//   xhr.onreadystatechange = function () {
-//     if (xhr.readyState !== 4) {
-//       return;
-//     }
-//     if (xhr.status >= 200 && xhr.status < 300) {
-//       // console.log('mainApp xhr');
-//     }
-//   };
-//   xhr.send(null);
+window.fetchM = function fetchM() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('get', `http://localhost:${ConfigCommon.mainPort}/mainApp`, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) {
+      return;
+    }
+    if (xhr.status >= 200 && xhr.status < 300) {
+      // console.log('mainApp xhr');
+    }
+  };
+  xhr.send(null);
 
-//   fetch(
-//     `http://localhost:${ConfigCommon.mainPort}/fetch/mainApp`,
-//   ).then((res) => {});
-// };
+  fetch(
+    `http://localhost:${ConfigCommon.mainPort}/fetch/mainApp`,
+  ).then((res) => {});
+};
 
-// window.unhandledrejectionError = function unhandledrejectionError() {
-//   setTimeout(() => {
-//     throw Error('mainApp: unhandledrejection error');
-//   }, 500);
-// };
+window.unhandledrejectionError = function unhandledrejectionError() {
+  setTimeout(() => {
+    throw Error('mainApp: unhandledrejection error');
+  }, 500);
+};
 
-// window.normalError = function normalError() {
-//   throw Error('mainApp: normal error');
-// };
+window.normalError = function normalError() {
+  throw Error('mainApp: normal error');
+};
 
-// window.DynamicResource = function DynamicResource() {
-//   const sc = document.createElement('script');
-//   sc.src = `http://localhost:${ConfigCommon.mainPort}/monitoring/dynamicScript.js`;
-//   document.body.appendChild(sc);
+window.DynamicResource = function DynamicResource() {
+  const sc = document.createElement('script');
+  sc.src = `http://localhost:${ConfigCommon.mainPort}/monitoring/dynamicScript.js`;
+  document.body.appendChild(sc);
 
-//   const link = document.createElement('link');
-//   link.href = `http://localhost:${ConfigCommon.mainPort}/monitoring/dynamicLink.css`;
-//   document.body.appendChild(link);
-// };
+  const link = document.createElement('link');
+  link.href = `http://localhost:${ConfigCommon.mainPort}/monitoring/dynamicLink.css`;
+  document.body.appendChild(link);
+};
 
-// window.resourceError = function resourceError() {
-//   // resource error
-//   const sc = document.createElement('script');
-//   sc.src = `http://localhost:1111/monitoring/xxxxx.js`;
-//   document.body.appendChild(sc);
-// };
+window.resourceError = function resourceError() {
+  // resource error
+  const sc = document.createElement('script');
+  sc.src = `http://localhost:1111/monitoring/xxxxx.js`;
+  document.body.appendChild(sc);
+};
