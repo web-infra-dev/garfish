fb2a0c0ca5cd750a2c0f369b0ff9a0908b48b293
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2021-10-15 11:19:18 +0800

    feat(core): - workflow: add debug command for garfish iife resource - core: use eventemitter2 repalce node events

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,25 +3,34 @@ import { replace } from 'esbuild-plugin-replace';
 
 const watch = process.env.WATCH;
 const sourcemap = Boolean(process.env.SOURCEMAP);
+const ignoreGlobalName = Boolean(process.env.IGNORE_GLOBAL_NAME);
 
-export const baseTsup = (pkg): Options => ({
-  sourcemap,
-  clean: true,
-  dts: true,
-  watch: watch ? 'src/' : false,
-  format: ['esm', 'cjs', 'iife'],
-  legacyOutput: true,
-  globalName: pkg.name
-    .replace(/@/g, '')
-    .split(/[\/-]/g)
-    .map((l) => l[0].toUpperCase() + l.slice(1))
-    .join(''),
-  esbuildPlugins: [
-    replace({
-      __TEST__: 'false',
-      __VERSION__: `'${pkg.version}'`,
-      __DEV__:
-        '(typeof process !== "undefined" && process.env && process.env.NODE_ENV ? (process.env.NODE_ENV !== "production") : false)',
-    }),
-  ],
-});
+export const baseTsup = (pkg): Options => {
+  const options: Options = {
+    sourcemap,
+    clean: true,
+    dts: true,
+    watch: watch ? 'src/' : false,
+    format: ['esm', 'cjs', 'iife'],
+    legacyOutput: true,
+    globalName: pkg.name
+      .replace(/@/g, '')
+      .split(/[\/-]/g)
+      .map((l) => l[0].toUpperCase() + l.slice(1))
+      .join(''),
+    esbuildPlugins: [
+      replace({
+        __TEST__: 'false',
+        __VERSION__: `'${pkg.version}'`,
+        __DEV__:
+          '(typeof process !== "undefined" && process.env && process.env.NODE_ENV ? (process.env.NODE_ENV !== "production") : false)',
+      }),
+    ],
+  };
+
+  // Can be directly by chrome plug-ins debugging injected garfish page
+  if (ignoreGlobalName) {
+    delete options.globalName;
+  }
+  return options;
+};
