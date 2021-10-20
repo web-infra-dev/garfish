c853f9b8284b496eb9aacc1a30ad12c13b78f153
chentao.arthur <chentao.arthur@bytedance.com>
2021-10-20 21:29:11 +0800

    chore: modify jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@
 
 module.exports = {
   clearMocks: true,
+  testEnvironment: 'jsdom',
   preset: 'jest-puppeteer',
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['__tests__', '/node_modules/'],
