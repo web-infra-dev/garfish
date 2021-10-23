4a2bb2a8e31f4ec7ac3ba5bf9b108690cb8e0065
chentao.arthur <chentao.arthur@bytedance.com>
2021-10-23 18:25:13 +0800

    chore(husky): create new config

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const chalk = require('chalk');
+console.log(process.env);
 const msgPath = process.env.HUSKY_GIT_PARAMS;
 const msg = fs.readFileSync(msgPath, 'utf-8').trim();
 const ng =
   'https://docs.google.com/document/d/1QrDFcIiPjSLDn3EL15IJygNPiHORgU1_OOAqWjiDU5Y/edit#heading=h.greljkmo14y0';
-const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/;
+const commitRE =
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/;
 
 if (!commitRE.test(msg)) {
   console.log();
