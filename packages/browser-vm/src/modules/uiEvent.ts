af84b4e75f5a51a68c77a2a6a9496e432eaf36be
chentao.arthur <chentao.arthur@bytedance.com>
2021-11-02 21:40:31 +0800

    style: merge code and adjust code style

--- packages/browser-vm/src/modules/uiEvent.ts
+++ packages/browser-vm/src/modules/uiEvent.ts
@@ -1,10 +1,9 @@
-import { _extends, getType } from '@garfish/utils';
+import { getType } from '@garfish/utils';
 
-/**
- * The logic of UIEvent is referenced from qiankun typography
- * https://github.com/umijs/qiankun/pull/593/files
- * TODO: fix normal mouse event instanceof MouseEvent === false
- */
+// The logic of UIEvent is referenced from qiankun typography
+// https://github.com/umijs/qiankun/pull/593/files
+// TODO: fix normal mouse event instanceof MouseEvent === false
+
 const RawMouseEvent = window.MouseEvent;
 export class MouseEventPatch extends RawMouseEvent {
   constructor(typeArg: string, mouseEventInit?: MouseEventInit) {
