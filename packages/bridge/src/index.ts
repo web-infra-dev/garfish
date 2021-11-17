d94e940fa6e66dac70f7511b883f272110aa9fef
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2021-11-17 10:18:42 +0800

    chore: modify Apache License to MIT license

---
+++ packages/bridge/src/index.ts
@@ -0,0 +1,3 @@
+import { def } from '@garfish/utils';
+
+function reactBridge() {}
--- packages/web-component/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="@garfish/browser-vm" />
-/// <reference types="@garfish/browser-snapshot" />
-/// <reference types="@garfish/router" />
-export * from './dist/index';
-import defaultInstance from './dist/index';
-
-export { default as Garfish } from '@garfish/core';
-export default defaultInstance;
