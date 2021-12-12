71e4ae40d68d6a95eadd328efc37625e62b21869
zhouxiao <codingzx@gmail.com>
2021-12-12 15:19:09 +0800

    chore(workflow): modify vite-plugin build config

--- packages/vite-plugin/tsup.config.ts
+++ packages/vite-plugin/tsup.config.ts
@@ -1,4 +1,8 @@
 import pkg from './package.json';
 import { baseTsup } from '../../tsup.config';
 
-export const tsup = baseTsup(pkg);
+const commonConfig = baseTsup(pkg);
+
+commonConfig.format = ['cjs', 'esm'];
+
+export const tsup = commonConfig;
