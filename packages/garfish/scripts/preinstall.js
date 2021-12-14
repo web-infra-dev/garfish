bfb8a4303d4abc8fcc71d1b4ed092aa3249e2c36
zhouxiao <codingzx@gmail.com>
2021-12-15 03:41:32 +0800

    chore(workflow): just warn internal no't install @byted/garfish

--- packages/garfish/scripts/preinstall.js
+++ packages/garfish/scripts/preinstall.js
@@ -1,18 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable no-restricted-globals */
 
-const os = require('os');
-const fs = require('fs');
-const path = require('path');
 const { spawnSync } = require('child_process');
-
-const tmpDirPath = os.tmpdir();
-const configPath = path.join(tmpDirPath, 'garfish-config.json');
-console.log('save Garfish config to tmp dir: ', tmpDirPath);
-
-const config = {
-  firstInstall: true,
-};
 
 // @byted/garfish preinstall 时会提供 fromInternalGarfish 环境变量
 const DISABLE_GARFISH_CHECK_INTERNAL =
@@ -21,15 +10,10 @@ const DISABLE_GARFISH_CHECK_INTERNAL =
 try {
   // 处于内网环境并且不是来自 @byted/garfish 隐式依赖时
   // 当前 Monorepo 仓库安装 demo 时
-  // 第一次安装 garfish 终止安装
   if (isInternal() && !DISABLE_GARFISH_CHECK_INTERNAL) {
-    if (!fs.existsSync(configPath)) {
-      fs.writeFileSync(configPath, JSON.stringify(config));
-      console.error(
-        '检测到目前可能处于内网环境，请使用 @byted/garfish 代替 garfish 包',
-      );
-      process.exit(1);
-    }
+    console.warn(
+      '检测到目前可能处于内网环境，请使用 @byted/garfish 代替 garfish 包',
+    );
   }
 } catch (err) {
   console.error(err.stack);
