dd563debfdc14506dccf85c9a57e356e8985fd97
Zhou xiao <zhouxiao.shaw@bytedance.com>
2022-01-08 12:05:22 +0800

    chore(workflow): modify codecov action (#371)
    * chore(workflow): modify codecov action and CI add cache pnpm

--- scripts/unit.js
+++ scripts/unit.js
@@ -7,6 +7,7 @@ step('🔎 Unit testing...');
   try {
     await run('jest', process.argv.slice(2, process.argv.length));
   } catch (error) {
+    console.error(error);
     process.exit(1);
   }
 })();
