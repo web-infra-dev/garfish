93b12929ad71bc0162583fa178735d41ce76f433
Arthur <chentao.arthur@bytedance.com>
2022-02-09 14:44:22 +0800

    fix: fix performance issue with computing resource size (#403)
    * fix: performance issue with computing resource size
    Co-authored-by: zhouxiao.shaw <codingzx@gmail.com>

--- packages/loader/src/utils.ts
+++ packages/loader/src/utils.ts
@@ -1,9 +1,4 @@
-import {
-  error,
-  isPrimitive,
-  isPlainObject,
-  parseContentType,
-} from '@garfish/utils';
+import { error, parseContentType } from '@garfish/utils';
 import { Manager, Loader } from './index';
 
 export async function request(url: string, config: RequestInit) {
@@ -14,8 +9,15 @@ export async function request(url: string, config: RequestInit) {
   }
   const code = await result.text();
   const type = result.headers.get('content-type');
+  const size = Number(result.headers.get('content-size'));
   const mimeType = parseContentType(type);
-  return { code, result, mimeType };
+
+  return {
+    code,
+    result,
+    mimeType,
+    size: Number.isNaN(size) ? null : size,
+  };
 }
 
 export function copyResult(result) {
@@ -30,28 +32,4 @@ export function mergeConfig(loader: Loader, url: string) {
   const extra = loader.requestConfig;
   const config = typeof extra === 'function' ? extra(url) : extra;
   return { mode: 'cors', ...config } as RequestInit;
-}
-
-export function calculateObjectSize(obj: any) {
-  let size = 0;
-  const valueSet = new WeakSet();
-  const add = (val: any) => {
-    if (isPrimitive(val)) {
-      size += new Blob([val]).size;
-    } else if (isPlainObject(val)) {
-      if (!valueSet.has(val)) {
-        valueSet.add(val);
-        for (const key in val) add(val[key]);
-      }
-    } else if (Array.isArray(val)) {
-      if (!valueSet.has(val)) {
-        valueSet.add(val);
-        val.forEach(add);
-      }
-    } else {
-      size += new Blob([val]).size;
-    }
-  };
-  add(obj);
-  return size;
 }
