c83da00631a496b971a26b83752fedd7b8ae9037
fengdanping <fengdanping@bytedance.com>
2022-02-17 12:12:08 +0800

    test(ci): vue2 build fail test

--- dev/app-angular/src/app/dashboard/dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-product-detail',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
-})
-export class DashboardComponent {}
