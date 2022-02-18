5fecde3f8f7aea6a59f6c69b89faaccdb4b92d25
fengdanping <fengdanping@bytedance.com>
2022-02-18 12:15:33 +0800

    test(ci): ci run test delete angular

--- dev/app-angular/src/app/pageNotFound/pageNotFound.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pageNotFound',
-  templateUrl: './pageNotFound.component.html',
-  styleUrls: ['./pageNotFound.component.css'],
-})
-export class PageNotFoundComponent {}
--- dev/app-angular/src/typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-export {};
-declare global {
-  interface Window {
-    __GARFISH__: boolean;
-    __GARFISH_EXPORTS__: any;
-  }
-}
