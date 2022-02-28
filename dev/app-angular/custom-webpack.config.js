2898cf08eb5346fb59bb7e29b366c932cc43f84a
danpeen <dapeen.feng@gmail.com>
2022-02-28 17:18:38 +0800

    fix(examples): fix angular router (#423)
    Co-authored-by: fengdanping <fengdanping@bytedance.com>

--- dev/app-angular/custom-webpack.config.js
+++ dev/app-angular/custom-webpack.config.js
@@ -17,11 +17,10 @@ module.exports = {
     // 若为 webpack4，此处应将 chunkLoadingGlobal 改为 jsonpFunction, 并确保每个子应用该值都不相同，否则可能出现 webpack chunk 互相影响的可能
     chunkLoadingGlobal: 'Garfish-demo-angular',
     // 保证子应用的资源路径变为绝对路径，避免子应用的相对资源在变为主应用上的相对资源，因为子应用和主应用在同一个文档流，相对路径是相对于主应用而言的
-    // publicPath: 'http://localhost:4200/',
     publicPath:
-      process.env.NODE_ENV === 'development'
-        ? `http://localhost:${port}/`
-        : publicPath,
+      process.env.NODE_ENV === 'production'
+        ? publicPath
+        : `http://localhost:${port}/`,
   },
   plugins: [
     new HtmlWebpackPlugin({
