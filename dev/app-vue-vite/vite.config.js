8c6c398867a0a8e308227c143c5d79ba031a45c0
fengdanping <fengdanping@bytedance.com>
2022-03-22 13:19:22 +0800

    fix(examples): fix vite e2etest fail

--- dev/app-vue-vite/vite.config.js
+++ dev/app-vue-vite/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { getPort } from '../util'
+import portMap from '../config.json';
+
 const appName = 'dev/vite';
+const port = portMap[appName].port;
 
 export default ({ mode }) => {
   process.env = {
@@ -10,13 +12,11 @@ export default ({ mode }) => {
   };
 
   return defineConfig({
-    base: `http://localhost:${getPort(appName)}`,
+    base: process.env.VITE_APP_BASE,
     server: {
-      port: getPort(appName),
+      port,
       cors: true,
-      origin: Boolean(process.env.IDE_PLATFORM)
-            ? `https://${getPort(appName)}-ssh2m4h1iyv4pprod.webide-boe.byted.org`
-            : `http://localhost:${getPort(appName)}`,
+      origin: `http://localhost:${port}`,
     },
     plugins: [
       vue(),
