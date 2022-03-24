cb3d810a17f713b2e7d80ba4e4ecad781e709dda
fengdanping <fengdanping@bytedance.com>
2022-03-24 09:24:57 +0800

    fix(core): delete useless code

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -63,22 +63,6 @@ export const generateAppOptions = (
   appOptionsOrUrl?: Omit<interfaces.AppInfo, 'name'>,
 ): AppInfo => {
   let appInfo = garfish.appInfos[appName];
-  // Load the unregistered applications
-  // `Garfish.loadApp('appName', 'https://xx.html');`
-  // if (typeof appOptionsOrUrl === 'string') {
-  //   if (appInfo) {
-  //     appInfo = {
-  //       ...appInfo,
-  //       entry: appOptionsOrUrl,
-  //     };
-  //   } else {
-  //     appInfo = {
-  //       name: appName,
-  //       basename: '/',
-  //       entry: appOptionsOrUrl,
-  //     };
-  //   }
-  // }
 
   // Merge register appInfo config and loadApp config
   if (isObject(appOptionsOrUrl)) {
