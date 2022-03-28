b129b553ff67830ea116fc8b4a35d993bb252702
fengdanping <fengdanping@bytedance.com>
2022-03-28 16:50:25 +0800

    chore(examples): get app information from Garfish instance in main App

--- dev/app-main/src/components/home/index.tsx
+++ dev/app-main/src/components/home/index.tsx
@@ -1,22 +1,21 @@
-import { observer } from "mobx-react";
-import { useState, useCallback, useEffect } from "react";
-import { Grid, Tabs, Spin } from "@arco-design/web-react";
-import { useNavigate } from "react-router-dom";
-import Garfish from "garfish";
-import MDEditor from "@uiw/react-md-editor";
-import AppLink from "../Link";
-import CardItem from "../CardItem";
-import { loadAppFunc } from "../loadApp/loadAppFunc";
+import { observer } from 'mobx-react';
+import { useState, useCallback, useEffect } from 'react';
+import { Grid, Tabs, Spin } from '@arco-design/web-react';
+import { useNavigate } from 'react-router-dom';
+import Garfish from 'garfish';
+import MDEditor from '@uiw/react-md-editor';
+import AppLink from '../Link';
+import CardItem from '../CardItem';
+import { loadAppFunc } from '../loadApp/loadAppFunc';
 import {
-	basicInfoStr_dev,
-	basicInfoStr_prod,
-	featuresStr,
-	toSubAppStr,
-	loadAppStr,
-	channelStr,
-	upgradeStr,
-} from "../mdStr";
-import "./index.less";
+  featuresStr,
+  toSubAppStr,
+  loadAppStr,
+  channelStr,
+  upgradeStr,
+  getAppBasicInfo,
+} from '../mdStr';
+import './index.less';
 
 const Row = Grid.Row;
 const Col = Grid.Col;
@@ -24,124 +23,126 @@ const Col = Grid.Col;
 const TabPane = Tabs.TabPane;
 
 const HomePage = observer(({ store }) => {
-	const navigate = useNavigate();
-	const [activeTab, setActiveTab] = useState("basicInfo");
-	const [text, setText] = useState("【主应用】手动挂载应用");
-	const [app, setApp] = useState<any>(null);
-	const [loadAsync, setLoadAsync] = useState(false);
+  const navigate = useNavigate();
+  const [bascInfo, setBascInfo] = useState('');
+  const [activeTab, setActiveTab] = useState('basicInfo');
+  const [text, setText] = useState('【主应用】动态加载应用');
+  const [app, setApp] = useState<any>(null);
+  const [loadAsync, setLoadAsync] = useState(false);
 
-	const loadApplication = useCallback(async () => {
-		setActiveTab("loadApp");
-		if (!app) {
-			setLoadAsync(true);
+  const loadApplication = useCallback(async () => {
+    setActiveTab('loadApp');
+    if (!app) {
+      setLoadAsync(true);
 
-			Garfish.router.push({
-				path: "/main/index/detail",
-				query: { id: "002" },
-			});
+      Garfish.router.push({
+        path: '/main/index/detail',
+        query: { id: '002' },
+      });
 
-			const app = await loadAppFunc({
-				id: "loadApp_vue2",
-				domID: "loadApp_mount",
-				appName: "vue2",
-				basename: "/examples/main/index",
-			});
+      const app = await loadAppFunc({
+        appName: 'vue2',
+        domID: 'loadApp_mount',
+        basename: '/examples/main/index',
+      });
 
-			setApp(app);
-			setLoadAsync(false);
-			setText("隐藏");
-		} else {
-			app.display ? setText("显示") : setText("隐藏");
-			app.display ? app.hide() : app.show();
-		}
-	}, [app]);
+      setApp(app);
+      setLoadAsync(false);
+      setText('隐藏');
+    } else {
+      app.display ? setText('显示') : setText('隐藏');
+      app.display ? app.hide() : app.show();
+    }
+  }, [app]);
 
-	useEffect(() => {
-		return () => {
-			app && app.hide();
-		};
-	}, [app]);
+  useEffect(() => {
+    return () => {
+      app && app.hide();
+    };
+  }, [app]);
 
-	return (
-		<Row
-			gutter={[24, 12]}
-			style={{
-				marginBottom: 16,
-				backgroundColor: "var(--color-fill-2)",
-			}}
-		>
-			<Col span={8} className="card-columns">
-				<CardItem
-					title="访问独立子应用"
-					href="https://garfish.top/quickStart"
-					content={
-						<div className="content-wrapper">
-							{store.apps.map((v: any) => (
-								<AppLink key={v.name} href={v.entry}>
-									{v.name}
-								</AppLink>
-							))}
-						</div>
-					}
-				/>
+  useEffect(() => {
+    window.Garfish.appInfos &&
+      Object.keys(window.Garfish.appInfos).length > 0 &&
+      setBascInfo(getAppBasicInfo(window.Garfish.appInfos));
+  }, [window.Garfish.appInfos]);
 
-				<CardItem
-					title="跳转子应用 react17 详情页"
-					onClick={() => {
-						// navigate(`/${basename}/react17`);
-						Garfish.router.push({
-							path: "/react17/detail",
-							query: { id: "002" },
-						});
-					}}
-					href="https://garfish.top/api/router/#routerpush"
-					markdownStr={toSubAppStr}
-				/>
+  return (
+    <Row
+      gutter={[24, 12]}
+      style={{
+        marginBottom: 16,
+        backgroundColor: 'var(--color-fill-2)',
+      }}
+    >
+      <Col span={8} className="card-columns">
+        <CardItem
+          title="访问子应用"
+          href="https://garfish.top/quickStart"
+          content={
+            <div className="content-wrapper">
+              {store.apps.map((v: any) => (
+                <AppLink key={v.name} href={v.entry}>
+                  {v.name}
+                </AppLink>
+              ))}
+            </div>
+          }
+        />
 
-				<CardItem
-					title={text}
-					onClick={loadApplication}
-					href="https://garfish.top/api/loadApp"
-					markdownStr={loadAppStr}
-				/>
+        <CardItem
+          title="跳转 react 子应用"
+          onClick={() => {
+            // navigate(`/${basename}/react17`);
+            Garfish.router.push({
+              path: '/react17/home',
+            });
+            // Garfish.router.push({
+            //   path: '/react17/detail',
+            //   query: { id: '002' },
+            // });
+          }}
+          href="https://garfish.top/api/router/#routerpush"
+          markdownStr={toSubAppStr}
+        />
 
-				<CardItem
-					title="与子应用通信"
-					onClick={() =>
-						window?.Garfish?.channel.emit("sayHello", "hello, i am main app")
-					}
-					href="https://garfish.top/api/channel"
-					markdownStr={channelStr}
-				/>
-			</Col>
+        <CardItem
+          title={text}
+          onClick={loadApplication}
+          href="https://garfish.top/api/loadApp"
+          markdownStr={loadAppStr}
+        />
 
-			<Col span={16}>
-				<Tabs activeTab={activeTab} onChange={setActiveTab}>
-					<TabPane key="basicInfo" title="example信息">
-						<MDEditor.Markdown
-							source={
-								process.env.NODE_ENV === "development"
-									? basicInfoStr_dev
-									: basicInfoStr_prod
-							}
-							linkTarget="_blank"
-						/>
-					</TabPane>
-					<TabPane key="features" title="已实现feature">
-						<MDEditor.Markdown source={featuresStr} linkTarget="_blank" />
-					</TabPane>
-					<TabPane key="loadApp" title="子应用挂载">
-						<div id="loadApp_mount">
-							{loadAsync && <Spin style={{ marginTop: "200px" }} />}
-						</div>
-					</TabPane>
-					<TabPane key="upgrade" title="如何升级">
-						<MDEditor.Markdown source={upgradeStr} linkTarget="_blank" />
-					</TabPane>
-				</Tabs>
-			</Col>
-		</Row>
-	);
+        <CardItem
+          title="与子应用通信"
+          onClick={() =>
+            window?.Garfish?.channel.emit('sayHello', 'hello, i am main app')
+          }
+          href="https://garfish.top/api/channel"
+          markdownStr={channelStr}
+        />
+      </Col>
+
+      <Col span={16}>
+        <Tabs activeTab={activeTab} onChange={setActiveTab}>
+          <TabPane key="basicInfo" title="example信息">
+            <MDEditor.Markdown source={bascInfo} linkTarget="_blank" />
+          </TabPane>
+          <TabPane key="features" title="已实现feature">
+            <MDEditor.Markdown source={featuresStr} linkTarget="_blank" />
+          </TabPane>
+          <TabPane key="loadApp" title="子应用挂载">
+            <div id="loadApp_mount">
+              {loadAsync && <Spin style={{ marginTop: '200px' }} />}
+            </div>
+          </TabPane>
+          <TabPane key="upgrade" title="如何升级">
+            <MDEditor.Markdown source={upgradeStr} linkTarget="_blank" />
+          </TabPane>
+        </Tabs>
+      </Col>
+    </Row>
+  );
 });
 
 export default HomePage;
