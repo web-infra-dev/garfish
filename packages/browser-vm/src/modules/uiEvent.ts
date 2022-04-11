9563a82b7a1af232de73b3b2a43b4fa9268ee21c
Zhou xiao <codingzx@gmail.com>
2022-04-11 15:59:50 +0800

    fix(vm-sandbox): Babel cannot correctly identify the MouseEvent  in the code as a native class (#454)

--- packages/browser-vm/src/modules/uiEvent.ts
+++ packages/browser-vm/src/modules/uiEvent.ts
@@ -3,9 +3,7 @@ import { getType } from '@garfish/utils';
 // The logic of UIEvent is referenced from qiankun typography
 // https://github.com/umijs/qiankun/pull/593/files
 // TODO: fix normal mouse event instanceof MouseEvent === false
-
-const RawMouseEvent = window.MouseEvent;
-export class MouseEventPatch extends RawMouseEvent {
+export class MouseEventPatch extends MouseEvent {
   constructor(typeArg: string, mouseEventInit?: MouseEventInit) {
     if (mouseEventInit && getType(mouseEventInit.view) === 'window') {
       mouseEventInit.view = window;
