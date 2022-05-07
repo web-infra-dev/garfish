332ab1eb2949d9eb1974eb8422c54d8509e8e19f
danpeen <dapeen.feng@gmail.com>
2022-05-07 16:02:10 +0800

    feat(examples): add react v18 subapp example (#481)
    * feat(examples): add react v18 subapp example
    
    * chore(example): update pnpm-lock.yaml
    
    * feat(example): update react v18 subapp example

---
+++ dev/app-react-18/webpack.config.js
@@ -0,0 +1,99 @@
+import { DefinePlugin } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { getPublicPath, isDevelopment,  getPort} from '../util'
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const appName = 'dev/react18';
+
+const webpackConfig = {
+  devtool: isDevelopment ? 'source-map' : false,
+  entry: {
+    main: './src/index.tsx',
+  },
+  output: {
+    // 开发环境设置 true 将会导致热更新失效
+    clean: isDevelopment ? false : true,
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
+    // 需要配置成 umd 规范
+    libraryTarget: 'umd',
+    // 修改不规范的代码格式，避免逃逸沙箱
+    globalObject: 'window',
+    // webpack5 此参数不是必须，webpack5 将会直接使用 package.json name 作为唯一值，请确保应用间的 name 各不相同
+    // 若为 webpack4，此处应将 chunkLoadingGlobal 改为 jsonpFunction, 并确保每个子应用该值都不相同，否则可能出现 webpack chunk 互相影响的可能
+    chunkLoadingGlobal: 'Garfish-demo-react18',
+    // 保证子应用的资源路径变为绝对路径，避免子应用的相对资源在变为主应用上的相对资源，因为子应用和主应用在同一个文档流，相对路径是相对于主应用而言的
+    publicPath: getPublicPath(appName)
+  },
+  node: false, // 避免 global 逃逸
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-typescript',
+            '@babel/preset-react',
+            '@babel/preset-env',
+          ],
+        },
+      },
+      {
+        test: /\.css$|\.less$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader',
+            options: { javascriptEnabled: true },
+          },
+        ],
+      },
+      {
+        test: /\.(svg|png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+        },
+      },
+      {
+        test: /\.woff|woff2|eot|ttf$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    // open: true,
+    historyApiFallback: true,
+    port:getPort(appName),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    allowedHosts: 'all',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ].concat(isDevelopment ? [new ReactRefreshWebpackPlugin()] : []),
+};
+export default webpackConfig;
