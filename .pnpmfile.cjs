b764b99ac92a9cc3c674bbc7d2650259c947e144
fengdanping <fengdanping@bytedance.com>
2022-05-30 19:54:47 +0800

    fix(docs): set react^17 to swiper

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,4 +1,5 @@
 function readPackage(pkg, context) {
+  // console.log('----pkg.name', pkg);
   // vue-template-compiler use vue as devDependency and use file protocol
   if (pkg.name === 'vue-template-compiler' && pkg.version.startsWith('2.')) {
     pkg.devDependencies = {
@@ -9,6 +10,15 @@ function readPackage(pkg, context) {
       vue: '^2.0.0',
     }
     context.log('vue => vue@2 in dependencies of vue-template-compiler');
+  }
+
+  if (pkg.name === 'swiper') {
+    pkg.dependencies = {
+      ...pkg.dependencies,
+      react: '^17',
+      'react-dom': '^17',
+    }
+    context.log('swiper => swiper in dependencies of react^17');
   }
   return pkg;
 }
