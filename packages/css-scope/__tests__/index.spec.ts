1026fd014c7183db507663037e92675a2b60174e
chentao.arthur <chentao.arthur@bytedance.com>
2022-08-04 11:49:38 +0800

    chore: adjust code

--- packages/css-scope/__tests__/index.spec.ts
+++
@@ -1,3 +0,0 @@
-describe('Css scope', () => {
-  it('needs tests', () => {});
-});
--- packages/css-scope/tsup.config.ts
+++
@@ -1,4 +0,0 @@
-import pkg from './package.json';
-import { baseTsup } from '../../tsup.config';
-
-export const tsup = baseTsup(pkg);
