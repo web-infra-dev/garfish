fe5926f15d4d84cf3a9fe1dad371153ec6f5ef11
fengdanping <fengdanping@bytedance.com>
2023-01-07 16:03:01 +0800

    fix(addSourceList): 测试2

--- packages/core/src/module/app.ts
+++ packages/core/src/module/app.ts
@@ -74,8 +74,10 @@ export class App {
   public cjsModules: Record<string, any>;
   public htmlNode: HTMLElement | ShadowRoot;
   public customExports: Record<string, any> = {}; // If you don't want to use the CJS export, can use this
-  public sourceList: Array<{ tagName: string; url: string | URL | Request }> = [];
-  public sourceListMap: Map<string, { tagName: string; url: string | URL | Request }> = new Map();
+  // public sourceList: Array<{ tagName: string; url: string | URL | Request }> = [];
+  // public sourceListMap: Map<string, { tagName: string; url: string | URL | Request }> = new Map();
+  public sourceList: Array<{ tagName: string; url: string }> = [];
+  public sourceListMap: Map<string, { tagName: string; url: string }> = new Map();
   public appInfo: AppInfo;
   public context: Garfish;
   public hooks: interfaces.AppHooks;
