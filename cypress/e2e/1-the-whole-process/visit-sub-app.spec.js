abe74685d39d138c63cff622fa9882084569bf50
zhouxiao.shaw <codingzx@gmail.com>
2024-06-19 11:12:25 +0800

    chore: fix e2e test

--- cypress/e2e/1-the-whole-process/visit-sub-app.spec.js
+++ cypress/e2e/1-the-whole-process/visit-sub-app.spec.js
@@ -148,7 +148,7 @@ describe('whole process app render', () => {
       cy.wait(4000);
       cy.contains('[data-test=title]', HomeTitle)
         .then(() => {
-          cy.get('[data-test=vite-count-btn]').click();
+          cy.get('[data-test=vite-count-btn]').click({ force: true });
           cy.contains('button', 'count is: 1');
         })
         .then(() => {
